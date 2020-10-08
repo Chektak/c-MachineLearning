9db64a80264ad8865993f481d8534fff2024364a
Chektak <com123com9115@gmail.com>
2020-10-08 20:23:40 +0900

    Matrix Test2

---
+++ 1005Matrix/1005Matrix.cpp
@@ -0,0 +1,112 @@
+﻿#include <iostream>
+#include <array>
+using namespace std;
+
+namespace Mathf {
+    //행(m) : 세로 줄, 열(n) : 가로로 얼마나 퍼져있는지
+    template<int mat1M, int mat1N, int mat2M, int mat2N>
+    void MatrixProduct(const std::array<std::array<double, mat1N>, mat1M> &mat1, 
+        const std::array<std::array<double, mat2N>, mat2M> &mat2,
+        std::array<std::array<double, mat2N>, mat1M> &resultMat
+    ) {
+        if (mat1M <= 0 || mat1N <= 0 || mat2M <= 0 || mat2N <= 0)
+        {
+            cout << "Can't Product!!";
+            return;
+        }
+        cout << "xInputMatrix" << endl;
+        //행
+        for (int i = 0; i < mat1M; i++) {
+            //열
+            for (int j = 0; j < mat1N; j++) {
+                cout << mat1[i][j] << "  ";
+            }
+            cout << endl;
+        }
+        cout << "\nwMatrix" << endl;
+        //행
+        for (int i = 0; i < mat2M; i++) {
+            //열
+            for (int j = 0; j < mat2N; j++) {
+                cout << mat2[i][j] << "  ";
+            }
+            cout << endl;
+        }
+
+        for (int z = 0; z < mat2N; z++) {
+            //행
+            for (int i = 0; i < mat1M; i++) {
+                //열
+                for (int j = 0; j < mat1N; j++) {
+                    resultMat[i][z] += mat1[i][j] * mat2[j][z];
+                }
+            }
+        }
+
+        cout << "\n결과 행렬 :: ResultMatrix" << endl;
+        //행
+        for (int i = 0; i < mat1M; i++) {
+            //열
+            for (int j = 0; j < mat2N; j++) {
+                cout << resultMat[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    /// <summary>
+    /// xMatrix * wMatrix = ResultMatrix 식에서 빈 wMatrix를 리턴한다
+    /// </summary>
+    /// <returns></returns>
+    template <int mat2N, int mat1M>
+    std::array<std::array<double, mat2N>, mat1M>* GetEmptyWMatrix() {
+        std::array <std::array<double, mat2N>, mat1M>* emptyMat
+            = new std::array <std::array<double, mat2N>, mat1M>;
+
+        std::array<double, mat2N> tempMat;
+        tempMat.fill(0);
+        emptyMat->fill(tempMat);
+
+        return emptyMat;
+    }
+};
+int main()
+{
+    //5행 3열 행렬( 5m 3n
+    std::array<std::array<double, 3>, 5> xMatrix = { 
+        1, 1, 1, 
+        2, 2, 2,
+        3, 3, 3,
+        1, 1, 1,
+        1, 1, 1
+    };
+    //3행 1열 행렬
+    std::array<std::array<double, 1>, 3> wMatrix = {   
+        1, 
+        2, 
+        3
+    };
+    //result : 5행 1열 행렬
+    std::array<std::array<double, 1>, 5> resultMatrix ={
+        0,
+        0,
+        0,
+        0,
+        0
+    };
+
+    Mathf::MatrixProduct<5, 3, 3, 1>(xMatrix, wMatrix, resultMatrix);
+    
+    std::array<std::array<double, 1>, 3>* wEmptyMatrix;
+    wEmptyMatrix = Mathf::GetEmptyWMatrix<1, 3>();
+    cout << "\nxMatrix와 resultMatrix로 w행렬을 추정해 빈 행렬 생성" << endl;
+    for (int i = 0; i < 1; i++) {
+        //열
+        for (int j = 0; j < 3; j++) {
+            printf("%.0lf\n", wEmptyMatrix[i][j]);
+        }
+        cout << endl;
+    }
+
+
+}
--- 1006LinearRegression_Matrix/1006LinearRegression_Matrix.cpp
+++ 1006LinearRegression_Matrix/1006LinearRegression_Matrix.cpp
@@ -1,24 +1,39 @@
 ﻿#include <iostream>
 #include <random>
 #include <array>
-#define TRAININGDATA_NUM 3
-#define TRAININGDATA_VARSIZE 3
+//훈련 데이터 x데이터, Y 행렬의 행(m) 개수
+#define TRAININGMATRIX_DATA_M 5
+//훈련 데이터 X데이터 행렬의 열(n) 개수
+#define TRAININGMATRIX_X_N 3
+//훈련 데이터 Y행렬 열(n) 개수
+#define TRAININGMATRIX_Y_N 1
+
 #define RANDOMRATE 50
 using namespace std;
 
 namespace Mathf {
-    template<int size>
-    double MatrixProduct(array<double, size>& rowArr, array<double, size>& columnArr) {
-        if (rowArr.empty() || columnArr.empty())
+    //행(m) : 세로 줄, 열(n) : 가로로 얼마나 퍼져있는지
+    template<int mat1M, int mat1N, int mat2M, int mat2N>
+    void MatrixProduct(const std::array<std::array<double, mat1N>, mat1M>& mat1,
+        const std::array<std::array<double, mat2N>, mat2M>& mat2,
+        std::array<std::array<double, mat2N>, mat1M>& resultMat) {
+
+        std::array<std::array<double, mat2N>, mat1M> resultMat = new std::array<std::array<double, mat2N>, mat1M>;
+        if (mat1M <= 0 || mat1N <= 0 || mat2M <= 0 || mat2N <= 0)
         {
-            std::cout << "Can't Product!!";
-            return 0;
+            cout << "Can't Product!!";
+            return resultMat;
         }
-        double result = 0;
-        for (int i = 0; i < size; i++) {
-            result += rowArr[i] * columnArr[i];
+        for (int z = 0; z < mat2N; z++) {
+            //행
+            for (int i = 0; i < mat1M; i++) {
+                //열
+                for (int j = 0; j < mat1N; j++) {
+                    resultMat[i][z] += mat1[i][j] * mat2[j][z];
+                }
+            }
         }
-        return result;
+        return resultMat;
     }
 };
 
@@ -28,17 +43,18 @@ random_device rd;
 mt19937 gen(rd());
 
 typedef struct datas {
-    array<double, TRAININGDATA_VARSIZE> w;
-    array<double, TRAININGDATA_VARSIZE> xData;
+    array<array<double, TRAININGMATRIX_X_N>, TRAININGMATRIX_DATA_M> xMatrix;
     double b;
-    double y;
-}TestDataSet;
+    array<array<double, TRAININGMATRIX_Y_N>, TRAININGMATRIX_DATA_M> yMatrix;
+}DataSet;
 
 class Machine
 {
 public:
-    array<double, TRAININGDATA_VARSIZE> bestW;
-    array<double, TRAININGDATA_VARSIZE> w;
+    DataSet trainingDataSet;
+
+    array<array<double, TRAININGMATRIX_Y_N>, TRAININGMATRIX_X_N> bestW;
+    array<array<double, TRAININGMATRIX_Y_N>, TRAININGMATRIX_X_N> wMatrix;
     double bestCost;
     double cost;
     double b;
@@ -48,28 +64,40 @@ public:
     double wRandomRate;
     double bRandomRate;
 
-    TestDataSet trainingDataSet[TRAININGDATA_NUM] = 
-    { {{1, 0, 1},  {73, 80, 75}, 0},
-      {{1, 0, 1},  {93, 88, 93}, 0},
-      {{1, 0, 1},  {89, 91, 90}, 0} };
 
 public:
-    Machine() : bestW({ 0 }), w({ 0 }), bestCost(DBL_MAX), cost(DBL_MAX), b(0), sameCostStack(0), wRandomRate(RANDOMRATE), bRandomRate(RANDOMRATE) {};
+    Machine() : bestW({ 0 }), wMatrix({ 0 }), bestCost(DBL_MAX), cost(DBL_MAX), b(0), sameCostStack(0), wRandomRate(RANDOMRATE), bRandomRate(RANDOMRATE) {};
 
+    void TrainingDataSetup() {
+        trainingDataSet.xMatrix = {
+            73, 80, 75,
+            93, 88, 93,
+            89, 91, 90,
+            96, 98, 100,
+            73, 66, 70
+        };
+        trainingDataSet.yMatrix = {
+            152,
+            185,
+            180,
+            196,
+            142
+        };
+    }
     /// <summary>
     /// </summary>
     /// <returns>cost를 반환한다.</returns>
     double Training(int learningRate, int curRepeatNum) {
-        //uniform_real_distribution<double> dis(bestW - wRandomRate, bestW + wRandomRate);
         uniform_real_distribution<double> dis(-1, 1);
 
-        //w = dis(gen);
-        for (int i = 0; i < TRAININGDATA_VARSIZE; i++)
-            w[i] = 0;
+        for (int i = 0; i < TRAININGMATRIX_Y_N; i++)
+            for(int j=0; j < TRAININGMATRIX_X_N; j++)
+                wMatrix[i][j] = dis(gen);
         cost = Cost();
 
         //코스트 줄이기에 성공했다면
-        if (cost < bestCost) {bestW = w;
+        if (cost < bestCost) {
+            bestW = wMatrix;
             bestCost = cost;
             //같은 코스트 판별 스택 초기화
             sameCostStack = 0;
@@ -93,61 +121,97 @@ public:
 
     double Cost() {
         double cost = 0;
-        for (int i = 0; i < TRAININGDATA_NUM; i++) {
-            cost += Loss(trainingDataSet[i].xData, trainingDataSet[i].y);
-        }
-        cost /= TRAININGDATA_NUM;
+        //for (int i = 0; i < TRAININGMATRIX_DATA_M; i++) {
+        //    cost += Loss<>(i);
+        //}
+        //cost /= TRAININGMATRIX_DATA_M;
+        cost = Loss<TRAININGMATRIX_DATA_M, TRAININGMATRIX_X_N, 
+            TRAININGMATRIX_X_N, TRAININGMATRIX_Y_N>();
         return cost;
     }
 
     /// <summary>
     /// Loss : (가설 H = wx+b) - 정답데이터 y
     /// </summary>
-    /// <param name="xInput"></param>
-    /// <param name="yTestData"></param>
     /// <returns></returns>
-    template<int size>
-    double Loss(array<double, size>& xInputArr, double yTestData) {
-        double loss = GetH<size>(w, xInputArr, b) - yTestData;
+    template<int xMatM, int xMatN, int wMatM, int wMatN>
+    double Loss() {
+        //y = H(X) = XW + b에서 y 행렬를 반환한다. (XW는 행렬곱)
+        array<array<double, wMatN>, xMatM> tempYMatrix;
+        Mathf::MatrixProduct<xMatM, xMatN, wMatM, wMatN>(trainingDataSet.xMatrix, wMatrix, tempYMatrix);
+        
+        double hData = 0;
+        double yData = 0;
+        for (int i = 0; i < xMatM; i++) {
+            hData += tempYMatrix[i][0];
+            yData += trainingDataSet.yMatrix[i][0];
+        }
+
+        cout << "\n결과 행렬 :: tempYMatrix" << endl;
+        //행
+        for (int i = 0; i < xMatM; i++) {
+            //열
+            for (int j = 0; j < xMatN; j++) {
+                cout << tempYMatrix[i][j] << " ";
+            }
+            cout << endl;
+        }
+        
+        cout << "\n정답 행렬 :: ResultMatrix" << endl;
+        for (int i = 0; i < wMatM; i++) {
+            //열
+            for (int j = 0; j < wMatN; j++) {
+                cout << trainingDataSet.yMatrix[i][j] << " ";
+            }
+            cout << endl;
+        }
+        double loss = hData - yData;
+        cout << loss << endl;
         loss *= loss;
         return loss;
     }
 
     /// <summary>
-    /// H(X) = XW + b에서 가설 H를 반환한다. (XW는 행렬곱)
+    /// y = H(X) = XW + b에서 y 행렬를 반환한다. (XW는 행렬곱)
     /// </summary>
-    /// <param name="xArr">영향요인(x) 배열</param>
-    /// <param name="wArr">가중치 배열</param>
-    /// <param name="b">절편</param>
+    /// <param name="wMatrix">가중치 행렬</param>
     /// <returns></returns>
-    template<int size>
-    double GetH(array<double, size>& xArr, array<double, size>& wArr, double b) {
-        return Mathf::MatrixProduct<size>(xArr, wArr) + b;
-    }
-
+   /* template<int mSize, int nSize>
+    double GetHMatrix(array<array<double, nSize>, mSize>& yMatrix, int index) {
+        return Mathf::MatrixProduct<size>(xArr, yMatrix) + b;
+    }*/
 };
 
 int main()
 {
     Machine* machine = new Machine();
-    machine->wRandomRate = RANDOMRATE;
+    machine->trainingDataSet = {
+        //xMatrix 초기화
+        {
+            73, 80, 75,
+            93, 88, 93,
+            89, 91, 90,
+            96, 98, 100,
+            73, 66, 70
+        },
+        //b
+        0,
+        {
+            152,
+            185,
+            180,
+            196,
+            142
+        }
+        //yMatrix 초기화
+    };
 
     int trainingNum = 0;
     int learningRate = 0;
 
     std::cout << "\n\nH(X) = WX + b 다변수 선형회귀 문제 훈련" << endl;
 
-    TestDataSet * dataSet = machine->trainingDataSet;
-    //dataSet의 y는 자동으로 계산해 넣음
-    for (int i = 0; i < TRAININGDATA_NUM; i++) {
-        dataSet[i].y = machine->GetH(dataSet[i].w, dataSet[i].xData, dataSet[i].b);
-        std::cout << "\tDataSet " << i + 1 <<endl;
-        for (int j = 0; j < TRAININGDATA_VARSIZE; j++) {
-            std::cout << "\t\t { w[" << j << "], x[" << j << "], b, Y } = ";
-            std::cout << dataSet[i].w[j] << ", " << dataSet[i].xData[j];
-            std::cout << ", " << dataSet[i].b << ", " << dataSet[i].y << endl;
-        }
-    }
+    DataSet * dataSet = &machine->trainingDataSet;
     std::cout << "훈련 횟수를 입력해주세요 : ";
     cin >> trainingNum;
 
@@ -159,37 +223,42 @@ int main()
     std::cout << fixed;
     //double형 최대 보장 범위인 소수점 15자리 이상까지 출력
     std::cout.precision(15);
-
+    machine->Cost();
+    //double yData = 0;
+    //for (int i = 0; i < TRAININGMATRIX_DATA_M; i++) {
+    //    yData += machine->trainingDataSet.yMatrix[i][0];
+    //}
+    //cout << yData << endl;
     //y절편은 기울기에 0을 곱하는 것으로 구할 수 있음
-    std::array<double, TRAININGDATA_VARSIZE> zeroSet;
-    zeroSet.fill(0);
-    machine->b = machine->GetH<TRAININGDATA_VARSIZE>(zeroSet, dataSet[0].xData, dataSet[0].b);
+    //std::array<double, TRAIN> zeroSet;
+    //zeroSet.fill(0);
+    //machine->b = machine->GetH<TRAININGDATA_VARSIZE>(zeroSet, dataSet[0].xData, dataSet[0].b);
     
-    for (int curRepeatNum = 0; curRepeatNum <= trainingNum; curRepeatNum++) {
-        double bestCost = machine->Training(learningRate, curRepeatNum);
-        array<double, TRAININGDATA_VARSIZE> bestW = machine->bestW;
-    
-        //소수점 15자리 이상까지 반올림
-        //bestCost = roundl(bestCost * 100000000000000) * 0.00000000000001;
-        //w의 소수점 자릿수가 6 이하일 때 cost 소수점 15자리 이하에 영향을 주므로 6자리에서 반올림
-        //bestW = roundl(bestW * 1000000) * 0.000001;
-    
-        //100번마다 출력하거나, 코스트가 0이 되었을 때 출력
-        if (curRepeatNum % 100 == 0 || bestCost <= 0) {
-            std::cout << "------------------------------------------------" << endl;
-            std::cout << curRepeatNum << "번째 세대" << endl;
-    
-            std::cout << "BestW : " << bestW[0] << endl;
-            std::cout << "b : " << machine->b << endl;
-            std::cout << "BestCost : " << bestCost << endl;
-    
-            if (bestCost == 0)
-                break;
-            else if (bestCost == -1) {
-                std::cout << "마지막 BestCost : " << machine->bestCost << endl;
-                break;
-            }
-        }
-    }
+    //for (int curRepeatNum = 0; curRepeatNum <= trainingNum; curRepeatNum++) {
+    //    double bestCost = machine->Training(learningRate, curRepeatNum);
+    //    array<double, TRAININGDATA_VARSIZE> bestW = machine->bestW;
+    //
+    //    //소수점 15자리 이상까지 반올림
+    //    //bestCost = roundl(bestCost * 100000000000000) * 0.00000000000001;
+    //    //w의 소수점 자릿수가 6 이하일 때 cost 소수점 15자리 이하에 영향을 주므로 6자리에서 반올림
+    //    //bestW = roundl(bestW * 1000000) * 0.000001;
+    //
+    //    //100번마다 출력하거나, 코스트가 0이 되었을 때 출력
+    //    if (curRepeatNum % 100 == 0 || bestCost <= 0) {
+    //        std::cout << "------------------------------------------------" << endl;
+    //        std::cout << curRepeatNum << "번째 세대" << endl;
+    //
+    //        std::cout << "BestW : " << bestW[0] << endl;
+    //        std::cout << "b : " << machine->b << endl;
+    //        std::cout << "BestCost : " << bestCost << endl;
+    //
+    //        if (bestCost == 0)
+    //            break;
+    //        else if (bestCost == -1) {
+    //            std::cout << "마지막 BestCost : " << machine->bestCost << endl;
+    //            break;
+    //        }
+    //    }
+    //}
     return 0;
 }
